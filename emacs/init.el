e91a7c02a7e94305af4ab71c532c4cfff7bf70a5
Andy Friesen <andy@imvu.com>
2014-01-20 23:29:57 -0800

    M-x revert-all-buffers

--- emacs/init.el
+++ emacs/init.el
@@ -164,6 +164,15 @@
 
 (require 'haskell-style)
 
+(defun revert-all-buffers ()
+    "Refreshes all open buffers from their respective files."
+    (interactive)
+    (dolist (buf (buffer-list))
+      (with-current-buffer buf
+        (when (and (buffer-file-name) (not (buffer-modified-p)))
+          (revert-buffer t t t) )))
+    (message "Refreshed open files.") )
+
 ;; From the ghc-mod instructions
 (autoload 'ghc-init "ghc" nil t)
 
